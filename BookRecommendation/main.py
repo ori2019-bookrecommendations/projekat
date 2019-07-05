e51037fc89b334798242cad988879cb858a1b05f
Nemanja <nemanja997@gmail.com>
2019-07-05 16:28:15 +0200

    Zavrsen Collaborative filtering, dodat SVD i NeuMF model

--- BookRecommendation/main.py
+++ BookRecommendation/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from BookRecommendation.recommendation_algorithms import popularity
+from sklearn.model_selection import train_test_split
+from BookRecommendation.recommendation_algorithms import popularity, collaborativeFiltering
 
 
 def load_data():
@@ -13,20 +14,49 @@ def load_data():
     return books, book_tags, ratings, tags, to_read, users
 
 
-def data_cleanup(ratings):
+def create_training_and_test_sets(ratings, split_percentage):
+    # Splits the ratings into a training and test set
+    train_df, test_df = train_test_split(ratings, stratify=ratings['user_id'], test_size=split_percentage)
+    return train_df, test_df
+
+
+def load_test_and_train_data():
+    # Loads each of the .csv input files and creates a data frame consisting of user IDs
+    test = pd.read_csv('./outputs/test_set.csv')
+    train = pd.read_csv('./outputs/train_set.csv')
+    return test, train
+
+
+def data_cleanup(ratings, tags, book_tags):
     # Cleans duplicates and uses the last known rating as the correct value
     ratings = ratings.drop_duplicates(subset=["user_id", "book_id"], keep="last")
 
-    #TODO Remove foreign tags
+    # Cleans tags that contain non ascii characters
+    tag_ids = tags[tags['tag_name'].map(lambda x: not all(ord(c) < 128 for c in x))].tag_id
+    tags.drop(tag_ids, inplace=True)
+
+    # Cleans rows that contain removed tag
+    row_index = book_tags[book_tags['tag_id'].map(lambda x: x in tag_ids)].index
+    book_tags.drop(row_index, inplace=True)
+
 
     #TODO Combine similar tags
 
-    return ratings
+    return ratings, tags, book_tags
+
+
+def save(ratings, tags, book_tags):
+    ratings.to_csv('./outputs/ratings.csv', index=False)
+    tags.to_csv('./outputs/tags.csv', index=False)
+    book_tags.to_csv('./outputs/book_tags.csv', index=False)
 
 
 if __name__ == "__main__":
     books, book_tags, ratings, tags, to_read, users = load_data()
-    ratings = data_cleanup(ratings)
+    ratings, tags, book_tags = data_cleanup(ratings, tags, book_tags)
+    save(ratings, tags, book_tags)
+
+    test, train = load_test_and_train_data()
 
     # Baseline models, not personalized in any way
     # Most popular
@@ -36,3 +66,26 @@ if __name__ == "__main__":
     # Highest rated
     recommendations = popularity.highest_rated(books, ratings)
     print(recommendations.head(n=10))
+
+    ncf = collaborativeFiltering.NeuralCollaborativeFiltering(ratings, 20, 30, 6)
+    svd = collaborativeFiltering.SVDCollaborativeFiltering(ratings)
+
+    ncf.load('./inputs/cf_models/ncf-model.cfm')
+    svd = svd.load('./inputs/cf_models/svd-model.cfm')
+
+    # Tests the models
+    ncf_mae, ncf_rmse = ncf.test_model(test, 131072)
+    print("NeuMF MAE: " + str(ncf_mae))
+    print("NeuMF RMSE: " + str(ncf_rmse))
+
+    svd_mae, svd_rmse = svd.test_model()
+    print("SVD MAE: " + str(svd_mae))
+    print("SVD RMSE: " + str(svd_rmse))
+
+    # Recommends books for an actual user
+    desired_user_id = 1
+    recommended_book_ids, predictions = ncf.predict(desired_user_id, books, ratings)
+    print(books[books['book_id'].isin(recommended_book_ids)][['title', 'authors']])
+
+    results = svd.predict(desired_user_id, books, ratings)
+    print(results.head(10)[['title', 'authors']])
---
+++ BookRecommendation/recommendation_algorithms/collaborativeFiltering.py
@@ -0,0 +1,183 @@
+import keras
+import numpy as np
+from surprise import Reader, Dataset, SVD, accuracy
+from surprise.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from math import sqrt
+import pickle
+
+
+class SVDCollaborativeFiltering:
+
+    # Based on Singular Value Decomposition (SVD) implementation built into surprise library
+    # Uses a matrix factorization method to reduce a matrix into lower dimension parts simplifying the calculations
+
+    def __init__(self, ratings):
+        # Surprise library does not allow using data frames as training and test set values
+        reader = Reader(rating_scale=(1, 5))
+        data = Dataset.load_from_df(ratings[['book_id', 'user_id', 'rating']], reader)
+
+        self.train, self.test = train_test_split(data, test_size=.20)
+        self.model = SVD()
+
+    def test_model(self):
+        # Checks the predicted values against the test set
+        # Returns Root Mean Square Error (RMSE) accuracy
+        predictions = self.model.test(self.test)
+        return accuracy.mae(predictions, verbose=False), accuracy.rmse(predictions, verbose=False)
+
+    def train_model(self):
+        # Trains the model on the training set (80% of the total ratings data)
+        self.model.fit(self.train)
+
+    def predict(self, user_id, books, ratings):
+        # Predicts recommended books for a given user
+
+        # Gets all unread books
+        already_read = ratings[ratings['user_id'] == user_id]['book_id'].unique()
+        prediction = books[['book_id', 'authors', 'title']].copy()
+        prediction = prediction.reset_index()
+        prediction = prediction[~prediction['book_id'].isin(already_read)]
+
+        # Predicts a rating for each book and sorts them
+        prediction['predict'] = prediction['book_id'].apply(lambda x: self.model.predict(user_id, x).est)
+        prediction = prediction.drop('book_id', axis=1)
+        prediction = prediction.sort_values('predict', ascending=False)
+        return prediction
+
+    def save(self, location):
+        # Fully saves the model
+        pickle.dump(self, open(location, 'wb'))
+
+    @staticmethod
+    def load(location):
+        # Loads the model
+        infile = open(location, 'rb')
+        obj = pickle.load(infile)
+        infile.close()
+        return obj
+
+
+class NeuralCollaborativeFiltering:
+
+    # Based on NeuMF model proposed by a paper "Neural Collaborative Filtering":
+    # https://dl.acm.org/citation.cfm?id=3052569
+
+    # This is a deep learning model combining GMF (General Matrix Factorization) and MLP (Multi-layer perceptron)
+    # predictions.
+
+    def __init__(self, ratings, lfu=8, lfb=10, lfmf=3):
+        # Hyper-parameters
+        self.num_latent_factors_user = lfu
+        self.num_latent_factors_book = lfb
+        self.num_latent_factors_matrix_factorization = lfmf
+
+        # Data frame input
+        self.ratings = ratings
+        self.num_users = len(self.ratings.user_id.unique())
+        self.num_books = len(self.ratings.book_id.unique())
+        self.model = self.create_model()
+
+    def create_model(self):
+        # Constructs the NeuMF model
+
+        # Inputs
+        book_input = keras.layers.Input(shape=[1], dtype='int32')
+        user_input = keras.layers.Input(shape=[1], dtype='int32')
+
+        # Matrix Factorization Embeddings
+        book_embedding_mf = keras.layers.Embedding(
+            input_dim=self.num_books + 1,
+            output_dim=self.num_latent_factors_matrix_factorization,)(book_input)
+        user_embedding_mf = keras.layers.Embedding(
+            name='mlp_embedding_user',
+            input_dim=self.num_users + 1,
+            output_dim=self.num_latent_factors_user)(user_input)
+
+        # Multi Layer Perceptron Embeddings
+        book_embedding_mlp = keras.layers.Embedding(
+            input_dim=self.num_books + 1,
+            output_dim=self.num_latent_factors_book)(book_input)
+        user_embedding_mlp = keras.layers.Embedding(
+            input_dim=self.num_users + 1,
+            output_dim=self.num_latent_factors_matrix_factorization)(user_input)
+
+        # Matrix Factorization latent vectors
+        book_vec_mf = keras.layers.Flatten()(book_embedding_mf)
+        book_vec_mf = keras.layers.Dropout(0.2)(book_vec_mf)
+
+        user_vec_mf = keras.layers.Flatten()(user_embedding_mlp)
+        user_vec_mf = keras.layers.Dropout(0.2)(user_vec_mf)
+
+        pred_mf = keras.layers.dot([book_vec_mf, user_vec_mf], axes=1, normalize=False)
+
+        # Multi Layer Perceptron latent vectors
+        book_vec_mlp = keras.layers.Flatten()(book_embedding_mlp)
+        book_vec_mlp = keras.layers.Dropout(0.2)(book_vec_mlp)
+
+        user_vec_mlp = keras.layers.Flatten()(user_embedding_mf)
+        user_vec_mlp = keras.layers.Dropout(0.2)(user_vec_mlp)
+
+        concat = keras.layers.concatenate([book_vec_mlp, user_vec_mlp])
+        concat_dropout = keras.layers.Dropout(0.2)(concat)
+
+        dense = keras.layers.Dense(200)(concat_dropout)
+        dense_batch = keras.layers.BatchNormalization()(dense)
+        dropout_1 = keras.layers.Dropout(0.2)(dense_batch)
+        dense_2 = keras.layers.Dense(100)(dropout_1)
+        dense_batch_2 = keras.layers.BatchNormalization()(dense_2)
+        dropout_2 = keras.layers.Dropout(0.2)(dense_batch_2)
+        dense_3 = keras.layers.Dense(50)(dropout_2)
+        dense_4 = keras.layers.Dense(20, activation='relu')(dense_3)
+
+        pred_mlp = keras.layers.Dense(1, activation='relu', name='Activation')(dense_4)
+
+        # Combination
+        combine_mlp_mf = keras.layers.concatenate([pred_mf, pred_mlp])
+        result_combine = keras.layers.Dense(200)(combine_mlp_mf)
+        deep_combine = keras.layers.Dense(100)(result_combine)
+
+        # Final result
+        result = keras.layers.Dense(1)(deep_combine)
+
+        # Create model with two inputs, one output
+        self.model = keras.Model([user_input, book_input], result)
+        opt = keras.optimizers.Adam(lr=0.001)
+        self.model.compile(optimizer=opt, loss='mean_squared_error')
+        return self.model
+
+    def test_model(self, test_set, batch):
+        # Tests the model performance against a test data frame
+        # Compares the predictions of the model with actual ratings
+        prediction = self.model.predict([test_set.user_id, test_set.book_id], batch_size=batch)
+        actual_rating = test_set.rating
+        return mean_absolute_error(actual_rating, prediction), sqrt(mean_squared_error(actual_rating, prediction)),
+
+    def train_model(self, training_set, batch, epochs_num, is_verbose, validation_split_percent):
+        # Trains the model on a given training data frame
+        # Depending on the hardware, different batch sizes will lead to faster fitting
+        self.model.fit([training_set.user_id, training_set.book_id], training_set.rating,
+                       batch_size=batch,
+                       epochs=epochs_num,
+                       verbose=is_verbose,
+                       validation_split=validation_split_percent)
+
+    def predict(self, user, books, ratings):
+        # Predicts top ten unread books for a given user ID
+        already_read = ratings[ratings['user_id'] == user]['book_id'].unique()
+        book_data = np.array(list(set(books[~books['book_id'].isin(already_read)].book_id)))
+        user_data = np.array([user for i in range(len(book_data))])
+
+        predictions = self.model.predict([user_data, book_data], batch_size=512)
+        predictions = np.array([item[0] for item in predictions])
+        recommended_book_ids = (-predictions).argsort()[:10]
+
+        return recommended_book_ids, predictions[recommended_book_ids]
+
+    def save(self, location):
+        # Fully saves the model
+        self.model.save(location)
+
+    def load(self, location):
+        # Loads the model
+        self.model = keras.models.load_model(location)
